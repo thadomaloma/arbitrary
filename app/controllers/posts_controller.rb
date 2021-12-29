5252dc9de164b87cb713cfd66a51b6878cbdcc9d
Thado Zalawmpuia <zlwmpthado@gmail.com>
2021-12-30 02:03:35 +0900

    DRY code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def index
@@ -13,11 +13,10 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def create
-    @post = Post.new(params.require(:post).permit(:content))
+    @post = Post.new(post_params)
     if @post.save
       flash[:notice] = "Post was created successfully."
       redirect_to @post
@@ -27,8 +26,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
-    if @post.update(params.require(:post).permit(:content))
+    if @post.update(post_params)
       flash[:notice] = "Post was updated successfully."
     else
       render 'edit'
@@ -36,9 +34,18 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 
 end
